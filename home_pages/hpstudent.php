9b7c7ace845499f0360f597719f88ef8ee94903a
cfcity <calebevanfrederick@gmail.com>
2025-02-16 22:36:08 -0400

    Fixing home tabs

--- home_pages/hpstudent.php
+++ home_pages/hpstudent.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="../css/styles.css">
 </head>
 
-<body style="background-color: rgb(63,63,63); height: 100vh;">
+<body style="background-color: rgb(63,63,63); background-size:cover;">
     <table>
         <tr>
             <td>
@@ -27,91 +27,24 @@
             <!-- Content / Tabs ------------------------------------------------------------------------------------------------ -->
             <td>
                 <div id="create" class="tabcontent">
-                    <div class="centerleft">
-                        <div class="homesides">
-                            <h4 class="centertop" style="font-family: Cambria; color: coral;">Library</h4>
-                            <button style="border-top: 2px solid;" class="vtabs"></button>
-                            <button class="vtabs" onclick="recentfile()"></button>
-                            <button class="vtabs" onclick="recentfile()"></button>
-                            <button class="vtabs" onclick="recentfile()"></button>
-                            <button class="vtabs" onclick="recentfile()"></button>
-                            <button style="border-bottom: 2px solid;" class="vtabs"></button>
-                        </div>
-                    </div>
-                    <div class="centerright">
-                        <div class="homesides">
-                            <button style="position: absolute; top: 7%; left: 15%;" class="sidebuttons" onclick="googledocs()">
-                                <img src="../images/googledocs.jpg" alt="Docs" class="sidebuttonsi">
-                            </button>
-                            <button style="position: absolute; top: 7%; right: 15%;" class="sidebuttons" onclick=""></button>
-                            <button class="centerbottom" style="position: absolute; top:95%; width: 10vw; height: 5vh; width: 100%; border-radius: 50px;" onclick="office()">Microsoft Office</button>
-                        </div>
-                    </div>
-                    <div class="center">
-                        <form action="saveto">
-                            <textarea class="homecenter" style="background-color: white; color: black;" name="" id=""></textarea>
-                        </form>
-                    </div>
+
                 </div>
 
                 <div id="home" class="tabcontent">
-                    <div class="center">
-                        <div class="homecenter">
-                            <table border="1" style="border-color: white;">
-                                <tr>
-
-                                    <td>
-                                        <button
-                                            style="background-image: linear-gradient(135deg, purple, rgb(255, 205, 255));"
-                                            class="servicebubbles"
-                                            onclick="spotifyopen()">
-                                            <img src="../images/Spotify-logo.png" alt="Spotify">
-                                        </button>
-                                    </td>
-
-                                    <td>
-                                        <button
-                                            style="background-image: linear-gradient(225deg, blue, rgb(205, 205, 250));"
-                                            class="servicebubbles"
-                                            onclick="elearning()">
-                                            <img src="../images/logo-main.png" alt="Salcc Logo">
-                                        </button>
-                                    </td>
-
-                                    <td>
-                                        <button
-                                            style="background-image: linear-gradient(45deg, orange, rgb(255, 237, 205));"
-                                            class="servicebubbles"
-                                            onclick="ebscohost()">
-                                            <img src="../images/EBSCO.png" alt="EBSCOhost logo">
-                                        </button>
-                                    </td>
-
-                                </tr>
-
-                                <tr>
-                                    <td>
-                                        <button
-                                            style="background-image: linear-gradient(315deg, red, rgb(255, 205, 205));"
-                                            class="servicebubbles"
-                                            onclick="youtube()">
-                                            <img src="../images/Youtube.png" alt="Spotify">
-                                        </button>
-                                    </td>
-
-                                    <td></td>
-
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                </tr>
-                            </table>
-
-                        </div>
-                    </div>
+                    <table border="1" width="100%" height="100%" style="text-align: center;">
+                        <tr height="10%">
+                            <td width="20%">Unknown</td>
+                            <td width="60%">Welcome</td>
+                            <td width="20%">Account</td>
+                        </tr>
+                        <tr>
+                            <td width="20%"> Important aspect <br> Most used</td>
+                            <td rowspan="2" colspan="2"> Important information</td>
+                        </tr>
+                        <tr>
+                            <td width="20%"> Last accesed</td>
+                        </tr>
+                    </table>
                 </div>
 
                 <div id="library" class="tabcontent">
@@ -136,50 +69,34 @@
                 </div>
 
                 <div id="services" class="tabcontent">
-                    <div class="center">
-                        <div class="homecenter">
-                            <table border="1" style="border-color: white;">
-                                <tr style="height: 90%">
-                                    <td>
-                                        <button
-                                            class="servicebubbles"
-                                            onclick="">
-                                            <img src="../images/Le_fort.jpeg" alt="le fort">
-                                        </button>
-                                    </td>
-
-                                    <td>
-                                        <button
-                                            class="servicebubbles"
-                                            onclick="">
-                                            <img src="../images/Student_services.png" alt="support">
-                                        </button>
-                                    </td>
-
-                                    <td>
-                                        <button
-                                            class="servicebubbles"
-                                            onclick="">
-                                            <img src="../images/departments.jpg" alt="">
-                                        </button>
-                                    </td>
-                                </tr>
-
-                                <tr style="height: 10%;">
-                                    <td><h3>Le Forte restaurant</h3></td>
-                                    <td><h3>Student Affairs</h3></td>
-                                    <td><h3>Departments Offices</h3></td>
-                                </tr>
-
-
-                        </div>
+                    <table border="1" width="100%" height="100%" style="text-align: center;">
+                        <tr height="10%">
+                            <td width="43%" colspan="2">Home</td>
+                            <td></td>
+                            <td width="20%">Account</td>
+                        </tr>
+                        <tr height="45%">
+                            <td width="10%">Services</td>
+                            <td width="33%"> Service 1 </td>
+                            <td width=34%> service 2</td>
+                            <td width="33%" > service 3 // more services </td>
+                        </tr>
+                        <tr>
+                        <td width="10%">inquiries</td>
+                            <td> inquiry 1 // service 4 </td>
+                            <td> inquiry 2 // service 5</td>
+                            <td> inquiry 3 // more inquiries <br> service 6</td>
+                        </tr>
+                    </table>
+                </div>
 
 
 
 
 
 
-                    </div>
+
+                </div>
                 </div>
                 </div>
 
