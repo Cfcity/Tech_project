b5c028c6f415693d547f99282e62e8f8ab1695b0
cfcity <calebevanfrederick@gmail.com>
2025-03-11 22:52:31 -0400

    fixtestphp

--- General/test.php
+++ General/test.php
@@ -1,4 +1,6 @@
 <?php
+session_start(); // Start the session
+
 // Initialize variables to avoid "undefined" errors
 $username = "";
 $email = "";
@@ -6,6 +8,7 @@ $password = "";
 $conpassword = "";
 $role = 3;
 $errors = 0;
+$replyto = [];
 
 // Connect to the database
 $db = mysqli_connect('localhost', 'root', '', 'test');
@@ -16,14 +19,15 @@ if (!$db) {
 
 // Check if form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['reg_user'])) {
+    // Reset errors
+    $errors = 0;
+
     // Sanitize and retrieve form data
     $username = mysqli_real_escape_string($db, $_POST['username']);
     $email = mysqli_real_escape_string($db, $_POST['email']);
     $password = mysqli_real_escape_string($db, $_POST['password']);
     $conpassword = mysqli_real_escape_string($db, $_POST['conpassword']);
     $role = mysqli_real_escape_string($db, $_POST['role']);
-
-
 
     // Validate form fields
     if (empty($username)) {
@@ -72,56 +76,50 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['reg_user'])) {
 }
 
 // Login - send data to database
-
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login_user'])) {
+    // Reset errors
+    $errors = 0;
 
     $username = mysqli_real_escape_string($db, $_POST['username']);
     $password = mysqli_real_escape_string($db, $_POST['password']);
 
-
     // Check data to see if it username and password matches  
     if ($errors == 0) {
-
         $query = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
         $results = mysqli_query($db, $query);
 
         if (mysqli_num_rows($results) == 1) {
-
+            $row = mysqli_fetch_assoc($results);
             $_SESSION['username'] = $username;
             $_SESSION['success'] = "Logged in";
+            $_SESSION['email'] = $row['email'];
+            $_SESSION['role'] = $row['role'];
+            $_SESSION['Id'] = $row['Id'];
 
-            session_start();
-            $_SESSION['username'] = $username;
-            $_SESSION['email'] = $email;
-            $_SESSION['role'] = $role;
-            $_SESSION['Id'] = $Id;
-            if ($role == 1) {
+            if ($row['role'] == 1) {
                 echo '<script type="text/javascript">';
                 echo 'window.open("../General/home.php", "_self");';
                 echo '</script>';
-            } else if ($role == 2) {
+            } else if ($row['role'] == 2) {
                 echo '<script type="text/javascript">';
-                echo 'window.open("../General/Faculty_signup.php", "_self");';
+                echo 'window.open("../General/hpfinance.php", "_self");';
                 echo '</script>';
-            } else if ($role == 3) {
+            } else if ($row['role'] == 3) {
                 echo '<script type="text/javascript">';
-                echo 'window.open("../General/student_signup.php", "_self");';
+                echo 'window.open("../General/hpstudent.php", "_self");';
                 echo '</script>';
             }
         } else {
-            echo "<p class = 'centertop'> Incorrect username / password</p>";
+            echo "<p class='centertop' style='z-index: 3; left: 25%'> Incorrect username / password</p>";
         }
     }
 }
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    $replyto = $_POST['replyto'];
-}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['reply'])) {
+    $replyto = $_POST['replyto'];
     echo '<script type="text/javascript">';
-    echo 'window.open("../Service_forms/Reply.php", "_self");';
+    echo 'window.open("../Service_forms/Reply.php?replyto=' . $replyto . '", "_self");';
     echo '</script>';
-    echo $replyto;
 }
 
 // Close the database connection
